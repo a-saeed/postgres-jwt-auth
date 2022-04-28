c6cd465932dc7395fa5d035d560a706774175cd1
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-04-28 04:19:45 +0200

    providing api routes

---
+++ app_api/routes/userRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import passport from "passport"
+
+import { login, register, protect } from "../controllers/userController.js";
+
+const userRouter = express.Router()
+
+
+userRouter.post('/register', register)
+userRouter.post('/login', login);
+
+/* -- protected route that can only be accessed by users with a valid token - */
+//userRouter.get('/protected', passport.authenticate('jwt', {session: false}), protect);
+
+
+export default userRouter;
